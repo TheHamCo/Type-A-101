ed091ed3f2d99c8b585fa25531ec327b6347a557
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 22:27:55 -0400

    feat: Add system timezone to datetimeformatter

--- app/src/main/java/co/dijam/michael/typea101/MyApplication.java
+++ app/src/main/java/co/dijam/michael/typea101/MyApplication.java
@@ -3,19 +3,34 @@ package co.dijam.michael.typea101;
 import android.app.Application;
 import android.content.Context;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  * Created by mdd23 on 9/6/2016.
  */
 public class MyApplication extends Application {
     private static Context context = null;
+    private static TimeZone timeZone = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+        timeZone = Calendar.getInstance().getTimeZone();
     }
 
     public static Context getContext(){
+        if (context == null){
+            context = this;
+        }
         return context;
+    }
+
+    public static TimeZone getTimeZone() {
+        if (timeZone == null){
+            timeZone = Calendar.getInstance().getTimeZone();
+        }
+        return timeZone;
     }
 }
--- app/src/main/java/co/dijam/michael/typea101/util/TimeFormattingUtil.java
+++ app/src/main/java/co/dijam/michael/typea101/util/TimeFormattingUtil.java
@@ -1,15 +1,20 @@
 package co.dijam.michael.typea101.util;
 
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 
+import co.dijam.michael.typea101.MyApplication;
+
 /**
  * Created by mdd23 on 9/6/2016.
  */
 public class TimeFormattingUtil {
-    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("h:mm:ss a");
+    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormat
+            .forPattern("h:mm:ss a")
+            .withZone(DateTimeZone.forTimeZone(MyApplication.getTimeZone()));
     public static final PeriodFormatter durationFormatter = new PeriodFormatterBuilder()
             .printZeroNever()
             .appendHours().appendSuffix(" hr ")
