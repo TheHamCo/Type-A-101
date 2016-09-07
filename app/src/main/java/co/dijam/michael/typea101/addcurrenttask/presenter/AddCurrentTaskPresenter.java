ad3288daa1f3583e4f9b326c952ee5eb8cfe9a7d
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 22:07:10 -0400

    feat: Persist current task in shared preferences

--- app/src/main/java/co/dijam/michael/typea101/addcurrenttask/presenter/AddCurrentTaskPresenter.java
+++ app/src/main/java/co/dijam/michael/typea101/addcurrenttask/presenter/AddCurrentTaskPresenter.java
@@ -1,20 +1,28 @@
 package co.dijam.michael.typea101.addcurrenttask.presenter;
 
 import co.dijam.michael.typea101.addcurrenttask.AddCurrentTaskContract;
+import co.dijam.michael.typea101.addcurrenttask.interactor.AddCurrentTaskInteractor;
+import co.dijam.michael.typea101.model.CurrentTask;
 
 /**
  * Created by mdd23 on 9/6/2016.
  */
 public class AddCurrentTaskPresenter implements AddCurrentTaskContract.Presenter {
     AddCurrentTaskContract.View view;
+    AddCurrentTaskInteractor interactor;
 
-    public AddCurrentTaskPresenter(AddCurrentTaskContract.View view) {
+    public AddCurrentTaskPresenter(AddCurrentTaskContract.View view, AddCurrentTaskInteractor interactor) {
         this.view = view;
+        this.interactor = interactor;
     }
 
     @Override
     public void setCurrentTask(String taskName, String tagName, long startTime) {
-        //TODO Persist current task information
+        CurrentTask currentTask = new CurrentTask();
+        currentTask.taskName = taskName;
+        currentTask.tag = tagName;
+        currentTask.startTime = startTime;
+        interactor.setOngoingTask(currentTask);
         view.closeAddTaskView();
     }
 }
