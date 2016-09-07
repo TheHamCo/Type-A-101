ad8cb1005a7baa5441ee45e608a822fdba665217
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 21:14:17 -0400

    feat: Stub AddCurrentTaskpresenter

---
+++ app/src/main/java/co/dijam/michael/typea101/addcurrenttask/presenter/AddCurrentTaskPresenter.java
@@ -0,0 +1,15 @@
+package co.dijam.michael.typea101.addcurrenttask.presenter;
+
+import co.dijam.michael.typea101.addcurrenttask.AddCurrentTaskContract;
+
+/**
+ * Created by mdd23 on 9/6/2016.
+ */
+public class AddCurrentTaskPresenter implements AddCurrentTaskContract.Presenter {
+    
+
+    @Override
+    public void setCurrentTask(String taskName, String tagName, long startTime) {
+
+    }
+}
