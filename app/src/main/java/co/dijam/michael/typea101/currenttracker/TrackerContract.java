c90f9097bc9500a9c36cd206a4ff2e107408aae7
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 22:38:35 -0400

    feat: Add TrackerContract

---
+++ app/src/main/java/co/dijam/michael/typea101/currenttracker/TrackerContract.java
@@ -0,0 +1,31 @@
+package co.dijam.michael.typea101.currenttracker;
+
+/**
+ * Created by mdd23 on 9/6/2016.
+ */
+public interface TrackerContract {
+    interface View {
+        void showTracker();
+        void hideTracker();
+
+        void showTaskName(String taskName);
+        void showTaskTag(String taskTagName);
+        void showTaskStartTime(String taskStartTime);
+
+        void updateTimer(String timerText);
+
+        void startEditTask();
+        void startAddNotes();
+    }
+
+    interface Presenter{
+        void getCurrentTask();
+        void runTimer();
+        void stopTimer();
+        void editCurrentTask();
+        void addNotesToCurrentTask();
+        void finishTracking;
+    }
+
+
+}
