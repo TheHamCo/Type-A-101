e43e036359a5279bfe283265c74e6ba6cd310bf3
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 21:39:27 -0400

    feat: Add basic data models

---
+++ app/src/main/java/co/dijam/michael/typea101/model/Task.java
@@ -0,0 +1,12 @@
+package co.dijam.michael.typea101.model;
+
+/**
+ * Created by mdd23 on 9/6/2016.
+ */
+public class Task {
+    public int id;
+    public String taskName;
+    public String tag;
+    public long startTime;
+    public long endTime;
+}
