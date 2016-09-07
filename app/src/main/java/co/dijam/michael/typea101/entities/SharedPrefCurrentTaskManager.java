1b25d76165ff8aab82c13a0097406a9e12b97b25
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 22:01:34 -0400

    feat: Implement SharedPrefCurrentTaskManager

---
+++ app/src/main/java/co/dijam/michael/typea101/entities/SharedPrefCurrentTaskManager.java
@@ -0,0 +1,52 @@
+package co.dijam.michael.typea101.entities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.icu.text.IDNA;
+
+import co.dijam.michael.typea101.model.CurrentTask;
+import co.dijam.michael.typea101.util.CurrentTaskConstantsUtil;
+import co.dijam.michael.typea101.util.SharedPrefConstantsUtil;
+
+import static co.dijam.michael.typea101.util.SharedPrefConstantsUtil.*;
+
+/**
+ * Created by mdd23 on 9/6/2016.
+ */
+public class SharedPrefCurrentTaskManager implements CurrentTaskManager {
+
+    SharedPreferences settings;
+
+    public SharedPrefCurrentTaskManager(Context context) {
+        settings = context.getSharedPreferences(CURRENT_TASK, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public CurrentTask getCurrentTask() {
+        CurrentTask currentTask = new CurrentTask();
+        currentTask.id = settings.getInt(ID, CurrentTaskConstantsUtil.NO_CURRENT_TASK_ID);
+        currentTask.taskName = settings.getString(TASK_NAME, "");
+        currentTask.tag = settings.getString(TAG, "");
+        currentTask.startTime = settings.getLong(START_TIME, 0);
+    }
+
+    @Override
+    public void setCurrentTask(CurrentTask currentTask) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(ID, currentTask.id);
+        editor.putString(TASK_NAME, currentTask.taskName);
+        editor.putString(TAG, currentTask.tag);
+        editor.putLong(START_TIME, currentTask.startTime);
+        editor.apply();
+    }
+
+    @Override
+    public void clearCurrentTask() {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(ID, CurrentTaskConstantsUtil.NO_CURRENT_TASK_ID);
+        editor.putString(TASK_NAME, "");
+        editor.putString(TAG, "");
+        editor.putLong(START_TIME, 0);
+        editor.apply();
+    }
+}
---
+++ app/src/main/java/co/dijam/michael/typea101/util/CurrentTaskConstantsUtil.java
@@ -0,0 +1,8 @@
+package co.dijam.michael.typea101.util;
+
+/**
+ * Created by mdd23 on 9/6/2016.
+ */
+public class CurrentTaskConstantsUtil {
+    public static final int NO_CURRENT_TASK_ID = -1;
+}
