8b0fbdfbe9d89a08ff868ee25d085d1798938ec5
Michael Dijamco <mdd233@cornell.edu>
2016-09-06 23:56:17 -0400

    feat: Add TrackerFragment to MainActivity

--- app/src/main/java/co/dijam/michael/typea101/MainActivity.java
+++ app/src/main/java/co/dijam/michael/typea101/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import co.dijam.michael.typea101.addcurrenttask.view.AddCurrentTaskActivity;
+import co.dijam.michael.typea101.currenttracker.view.TrackerFragment;
 import co.dijam.michael.typea101.entities.CurrentTaskManager;
 import co.dijam.michael.typea101.entities.SharedPrefCurrentTaskManager;
 import co.dijam.michael.typea101.util.TimeFormattingUtil;
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.add_task_button)
     FloatingActionButton addTaskButton;
+    @BindView(R.id.tracker_frame)
+    FrameLayout trackerFrame;
+    @BindView(R.id.list_frame)
+    FrameLayout listFrame;
+
+    private static final String FRAGMENT_TRACKER = "FRAGMENT_TRACKER";
+    TrackerFragment trackerFragment;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // LIFECYCLE
@@ -28,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        if (savedInstanceState == null){
+            trackerFragment = new TrackerFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.tracker_frame, trackerFragment, FRAGMENT_TRACKER).commit();
+        } else {
+            trackerFragment = (TrackerFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_TRACKER);
+        }
+
         CurrentTaskManager currentTaskManager = new SharedPrefCurrentTaskManager(getApplicationContext());
         String formattedTime = TimeFormattingUtil.dateTimeFormatter.print(currentTaskManager.getCurrentTask().startTime);
         Toast.makeText(MainActivity.this, currentTaskManager.getCurrentTask().toString() + formattedTime, Toast.LENGTH_SHORT).show();
@@ -37,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     // BUTTON BINDINGS
 
     @OnClick(R.id.add_task_button)
-    void onAddTaskButton(FloatingActionButton b){
+    void onAddTaskButton(FloatingActionButton b) {
         startActivity(new Intent(this, AddCurrentTaskActivity.class));
     }
 }
