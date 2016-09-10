4ab8cba0143c6f2db878b8e28cea71893759e704
Michael Dijamco <mdd233@cornell.edu>
2016-09-10 12:05:54 -0400

    feat: Task model extends RealmObject

--- app/src/main/java/co/dijam/michael/typea101/model/Task.java
+++ app/src/main/java/co/dijam/michael/typea101/model/Task.java
@@ -1,9 +1,13 @@
 package co.dijam.michael.typea101.model;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by mdd23 on 9/6/2016.
  */
-public class Task {
+public class Task extends RealmObject{
+    @PrimaryKey
     public int id;
     public String taskName;
     public String tag;
