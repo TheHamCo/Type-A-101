930b5c429b58c4703a75a7547d2760f878964104
Michael Dijamco <mdd233@cornell.edu>
2016-09-10 12:04:22 -0400

    feat: Create TaskManager contract

--- app/src/main/java/co/dijam/michael/typea101/entities/TaskManager.java
+++ app/src/main/java/co/dijam/michael/typea101/entities/TaskManager.java
@@ -1,7 +1,20 @@
 package co.dijam.michael.typea101.entities;
 
+import org.joda.time.DateTime;
+
+import co.dijam.michael.typea101.model.Task;
+import rx.Observable;
+
 /**
  * Created by mdd23 on 9/7/2016.
  */
 public interface TaskManager {
+    Observable<Task> getAllTasks();
+    Observable<Task> getAllTasksForOneDay(long dateTime);
+    Observable<Task> getTask(int id);
+
+    int insertTask(Task task);
+    void editTask(Task editedTask);
+    void deleteTask(int id);
+}
 }
