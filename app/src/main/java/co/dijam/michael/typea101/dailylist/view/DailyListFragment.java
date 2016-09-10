783974ece56ef73694ae0a869061fce40c37f18d
Michael Dijamco <mdd233@cornell.edu>
2016-09-10 16:34:11 -0400

    refactor: Remove debug logging for RecyclerView update

--- app/src/main/java/co/dijam/michael/typea101/dailylist/view/DailyListFragment.java
+++ app/src/main/java/co/dijam/michael/typea101/dailylist/view/DailyListFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -93,10 +92,6 @@ public class DailyListFragment extends Fragment implements DailyListContract.Vie
                 .subscribe(taskListItems1 -> {
                     taskListItems.clear();
                     taskListItems.addAll(taskListItems1);
-                    for (TaskListItem tli :
-                            taskListItems1) {
-                        Log.d("RECYCLER", "showTaskList: " + tli.taskName);
-                    }
                     adapter.notifyDataSetChanged();
                 });
     }
