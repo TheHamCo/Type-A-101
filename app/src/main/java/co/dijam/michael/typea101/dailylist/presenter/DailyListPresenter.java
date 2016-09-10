347741764b4ef1dcdcaf79e5ec3c6d30674efa11
Michael Dijamco <mdd233@cornell.edu>
2016-09-10 13:20:23 -0400

    feat: Implement DailyListPresenter

--- app/src/main/java/co/dijam/michael/typea101/dailylist/presenter/DailyListPresenter.java
+++ app/src/main/java/co/dijam/michael/typea101/dailylist/presenter/DailyListPresenter.java
@@ -1,13 +1,25 @@
 package co.dijam.michael.typea101.dailylist.presenter;
 
 import co.dijam.michael.typea101.dailylist.DailyListContract;
+import co.dijam.michael.typea101.dailylist.interactor.DailyListInteractor;
 
 /**
  * Created by mdd23 on 9/10/2016.
  */
 public class DailyListPresenter implements DailyListContract.Presenter {
+
+    DailyListContract.View view;
+    DailyListInteractor interactor;
+
+    public DailyListPresenter(DailyListContract.View view, DailyListInteractor interactor) {
+        this.view = view;
+        this.interactor = interactor;
+    }
+
     @Override
     public void getTaskListForDay(long dateTime) {
-
+        view.showTaskList(
+                interactor.getFormattedTaskListForDay(dateTime)
+        );
     }
 }
