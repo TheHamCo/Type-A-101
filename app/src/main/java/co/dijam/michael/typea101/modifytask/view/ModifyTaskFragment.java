d5daf104b797aa31de49e1772ee92b85eccde3b4
Michael Dijamco <mdd233@cornell.edu>
2016-09-15 10:43:09 -0400

    feat: ModifyTask xml bindings

--- app/src/main/java/co/dijam/michael/typea101/modifytask/view/ModifyTaskFragment.java
+++ app/src/main/java/co/dijam/michael/typea101/modifytask/view/ModifyTaskFragment.java
@@ -2,13 +2,24 @@ package co.dijam.michael.typea101.modifytask.view;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import co.dijam.michael.typea101.R;
 import co.dijam.michael.typea101.dailylist.model.TaskPrintable;
 import co.dijam.michael.typea101.modifytask.ModifyTaskContract;
@@ -18,6 +29,64 @@ import rx.Observable;
  * A simple {@link Fragment} subclass.
  */
 public class ModifyTaskFragment extends Fragment implements ModifyTaskContract.View {
+
+    // TASK NAME + TAG
+    @BindView(R.id.task_name_edit)
+    AutoCompleteTextView taskNameEdit;
+    @BindView(R.id.task_name_edit_layout)
+    TextInputLayout taskNameEditLayout;
+    @BindView(R.id.tag_edit)
+    AutoCompleteTextView tagEdit;
+    @BindView(R.id.tag_edit_layout)
+    TextInputLayout tagEditLayout;
+
+    // DATE
+    @BindView(R.id.date_text_view)
+    TextView dateTextView;
+    @BindView(R.id.day_edit_clickable)
+    FrameLayout dayEditClickable;
+
+    // START TIME
+    @BindView(R.id.starttime_text_view)
+    TextView starttimeTextView;
+    @BindView(R.id.start_time_picker_clickable)
+    FrameLayout startTimePickerClickable;
+    @BindView(R.id.start_time_count_label)
+    TextView startTimeCountLabel;
+    @BindView(R.id.start_time_existing_task_icon)
+    ImageView startTimeExistingTaskIcon;
+    @BindView(R.id.start_time_existing_task_clickable)
+    LinearLayout startTimeExistingTaskClickable;
+    @BindView(R.id.start_time_task_recycler)
+    RecyclerView startTimeTaskRecycler;
+    @BindView(R.id.expandable_start_time_list)
+    LinearLayout expandableStartTimeList;
+
+    // END TIME
+    @BindView(R.id.endtime_text_view)
+    TextView endtimeTextView;
+    @BindView(R.id.end_time_picker_clickable)
+    FrameLayout endTimePickerClickable;
+    @BindView(R.id.end_time_count_label)
+    TextView endTimeCountLabel;
+    @BindView(R.id.end_time_existing_task_icon)
+    ImageView endTimeExistingTaskIcon;
+    @BindView(R.id.end_time_existing_task_clickable)
+    LinearLayout endTimeExistingTaskClickable;
+    @BindView(R.id.end_time_task_recycler)
+    RecyclerView endTimeTaskRecycler;
+    @BindView(R.id.expandable_end_time_list)
+    LinearLayout expandableEndTimeList;
+
+    // DURATION
+    @BindView(R.id.duration_text_view)
+    TextView durationTextView;
+
+    // FINISH BUTTONS
+    @BindView(R.id.confirm_button)
+    ImageButton confirmButton;
+    @BindView(R.id.cancel_button)
+    ImageButton cancelButton;
 
     ModifyTaskContract.Presenter presenter;
 
@@ -32,7 +101,46 @@ public class ModifyTaskFragment extends Fragment implements ModifyTaskContract.V
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_modify_task, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // BUTTON BINDINGS
+
+    @OnClick(R.id.day_edit_clickable)
+    public void onDayEditClick() {
+    }
+
+    @OnClick({R.id.start_time_picker_clickable, R.id.start_time_existing_task_clickable})
+    public void onStartTimeClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_time_picker_clickable:
+                break;
+            case R.id.start_time_existing_task_clickable:
+                break;
+        }
+    }
+
+    @OnClick({R.id.end_time_picker_clickable, R.id.end_time_existing_task_clickable})
+    public void onEndTimeClick(View view) {
+        switch (view.getId()) {
+            case R.id.end_time_picker_clickable:
+                break;
+            case R.id.end_time_existing_task_clickable:
+                break;
+        }
+    }
+
+    @OnClick({R.id.confirm_button, R.id.cancel_button})
+    public void onConfirmCancelClick(View view) {
+        switch (view.getId()) {
+            case R.id.confirm_button:
+                break;
+            case R.id.cancel_button:
+                break;
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
